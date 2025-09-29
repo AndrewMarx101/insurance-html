4fdb8ab684bf7d047a3792211e7d92044b13d7ce
Andrew Marx <andrewmarx26@gmail.com>
2025-09-29 12:41:02 +0200

    upload

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 $(function () {
     if ($("header").length === 0) {
         // redirect back to index.html
-        window.location.href = "http://127.0.0.1:5500/index.html";
+        window.location.href = "https://andrewmarx101.github.io/insurance-html/";
     }
 });
